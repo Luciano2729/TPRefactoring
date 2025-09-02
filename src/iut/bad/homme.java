bda7c12194b34083146f797519e1eb103f020fd3
Luciano2729 <luciano.haisoa@gmail.com>
2025-09-02 11:48:36 +0200

    Création de la classe Humain (classe parente) Déplacement des attributs communs de Homme et Femme vers Humain

--- src/iut/bad/homme.java
+++ src/iut/bad/homme.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package iut.bad;
 
-/**
- *
- * @author RANDRIANASOLO
- */
-public class homme {
-    
+public class homme extends humain {
+    public homme(String nom, String prenom, int age) {
+        super(nom, prenom, age);
+    }
 }
+
---
+++ src/iut/bad/humain.java
@@ -0,0 +1,13 @@
+package iut.bad;
+
+public class humain {
+    protected String nom;
+    protected String prenom;
+    protected int age;
+
+    public humain(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}
