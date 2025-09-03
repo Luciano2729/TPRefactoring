425c565879609d6831e7e5d75f8a5085d82ab3e6
Luciano2729 <luciano.haisoa@gmail.com>
2025-09-03 06:56:37 +0200

    Ajout de la méthode main() dans femme

--- src/iut/bad/femme.java
+++ src/iut/bad/femme.java
@@ -4,5 +4,19 @@ public class femme extends humain {
     public femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
+    
+    public static void main(String[] args) {
+    homme h = new homme("Dupont", "Jean", 25);
+    femme f = new femme("Martin", "Sophie", 23);
+
+    h.details(); // affiche les infos de l’homme
+    f.details(); // affiche les infos de la femme
+
+    h.manger();  // "Jean mange."
+    f.boire();   // "Sophie boit."
+
+    f.ami(h);    // "Sophie est ami avec Jean"
+}
+
 }
 
