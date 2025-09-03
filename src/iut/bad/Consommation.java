ff0af578671cfe408967459cfc74643a3d400c4d
Luciano2729 <luciano.haisoa@gmail.com>
2025-09-03 05:44:27 +0200

    Interface Consommation

--- src/iut/bad/Consommation.java
+++ src/iut/bad/Consommation.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package iut.bad;
 
-/**
- *
- * @author RANDRIANASOLO
- */
 public interface Consommation {
-    
+    void manger();
+    void boire();
 }
